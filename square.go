6ed922c1d1df223de8e822619ea42b5fffe3f9b4
John Botero <John_Botero@epam.com>
2022-06-02 20:26:33 -0500

    Solution 0001

--- square.go
+++ square.go
@@ -9,14 +9,19 @@ type Square struct {
 	a     uint
 }
 
-func (receiver) End() Point {
+func (square Square) End() Point {
 	// implement me
+	endPoint := Point{x: square.start.x + int(square.a), y: square.start.y + int(square.a)}
+
+	return endPoint
 }
 
-func (receiver) Area() uint {
+func (square Square) Area() uint {
 	// implement me
+	return uint(square.a * square.a)
 }
 
-func (receiver) Perimeter() uint {
+func (square Square) Perimeter() uint {
 	// implement me
+	return uint(square.a * 4)
 }
--- square_test.go
+++ square_test.go
@@ -1,1 +1,35 @@
 package square
+
+import "testing"
+
+func TestArea(t *testing.T) {
+	square := Square{start: Point{x: 3, y: 5}, a: 3}
+
+	want := uint(9)
+	got := square.Area()
+
+	if got != want {
+		t.Errorf("TestArea got: %v, want: %v", got, want)
+	}
+}
+
+func TestPermiter(t *testing.T) {
+	square := Square{start: Point{x: 8, y: 9}, a: 5}
+
+	want := uint(20)
+	got := square.Perimeter()
+
+	if got != want {
+		t.Errorf("TestPermiter got: %v, want: %v", got, want)
+	}
+}
+
+func TestEnd(t *testing.T) {
+	square := Square{start: Point{x: 8, y: 9}, a: 5}
+	want := Point{x: 13, y: 14}
+	got := square.End()
+
+	if got != want {
+		t.Errorf("TestEnd got: %v, want: %v", got, want)
+	}
+}
